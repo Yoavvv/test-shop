a1bfc1d07c6a171d6c393021d04b5b2952bd4308
Yoav <yoav@github.com>
2018-02-15 11:08:51 +0200

    Function test commit

---
+++ wp-content/themes/oceanwp-child-theme/functions.php
@@ -0,0 +1,21 @@
+/*
+Theme Name: OceanWP Child
+Theme URI: https://oceanwp.org/
+Description: OceanWP WordPress theme example child theme.
+Author: Nick
+Author URI: https://oceanwp.org/
+Template: oceanwp
+Version: 1.0
+*/
+
+/* Parent stylesheet should be loaded from functions.php not using @import */
+
+<?php 
+
+function enqueue_parent_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+    wp_enqueue_style( 'rtl-style', get_template_directory_uri().'/rtl.css' );
+
+}
+
+add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
