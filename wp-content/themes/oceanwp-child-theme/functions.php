334157bf37624536328a2db4402512da98dcb068
Yoav <yoav@github.com>
2018-02-15 12:24:41 +0200

    3rd back commit

--- wp-content/themes/oceanwp-child-theme/functions.php
+++ wp-content/themes/oceanwp-child-theme/functions.php
@@ -10,12 +10,3 @@ Version: 1.0
 
 /* Parent stylesheet should be loaded from functions.php not using @import */
 
-<?php 
-
-function enqueue_parent_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.min.css' );
-    wp_enqueue_style( 'rtl-style', get_template_directory_uri().'/rtl.css' );
-
-}
-
-add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
